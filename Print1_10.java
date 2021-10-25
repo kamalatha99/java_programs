0755ebc4659f255f2e56868afe74b37d7b869302
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2021-10-25 14:08:22 +0530

    Add files via upload

---
+++ Print1_10.java
@@ -0,0 +1,19 @@
+package basics;
+
+public class Print1_10 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		for(int i=1;i<=10;i++)
+		{
+			System.out.print(i+" ");
+		}
+		System.out.println("\n");
+		for(int i=10;i>=1;i--)
+		{
+			System.out.print(i+" ");
+		}
+
+	}
+
+}
