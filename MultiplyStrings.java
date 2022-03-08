b09dd3b9097c206c9c11d8741c1803c25bb0e7ba
kamalatha99 <69569790+kamalatha99@users.noreply.github.com>
2022-03-08 13:29:09 +0530

    String

---
+++ MultiplyStrings.java
@@ -0,0 +1,19 @@
+package phase2;
+import java.util.*;
+public class MultiplyStrings {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner obj=new Scanner(System.in);
+		String s1=obj.next();
+		String s2=obj.next();
+		int s=Integer.parseInt(s1);
+		int ss=Integer.parseInt(s2);
+		int add=s*ss;
+		System.out.println(String.format("%d",add));
+		System.out.println(String.valueOf(add));
+		
+		
+	}
+
+}
